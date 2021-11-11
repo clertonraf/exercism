9796e856e843a6651a45adf4dfad3985a4328685
Clerton Araujo <clerton.ribeirodeara@doctolib.com>
2021-11-11 16:26:38 +0100

    saving account refactoring

--- ruby/savings-account/savings_account.rb
+++ ruby/savings-account/savings_account.rb
@@ -1,25 +1,27 @@
-module SavingsAccount
+# frozen_string_literal: true
 
+# SavingAccount module
+module SavingsAccount
   def self.interest_rate(balance)
     case balance
-      when -Float::INFINITY...0 then -3.213
-      when 0...1000 then 0.5
-      when 1000...5000 then 1.621
-      when 5000..Float::INFINITY then 2.475
-      else 0
+    when -Float::INFINITY...0 then -3.213
+    when 0...1000 then 0.5
+    when 1000...5000 then 1.621
+    when 5000..Float::INFINITY then 2.475
+    else 0
     end
   end
 
   def self.annual_balance_update(balance)
-    balance * (1.0 + (self.interest_rate(balance)/100).abs)
+    balance * (1.0 + (interest_rate(balance) / 100).abs)
   end
 
   def self.years_before_desired_balance(current_balance, desired_balance)
     years = 0
     until current_balance >= desired_balance
-      current_balance = self.annual_balance_update(current_balance)
+      current_balance = annual_balance_update(current_balance)
       years += 1
     end
-    return years
+    years
   end
 end
